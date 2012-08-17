c400d055b2987ffac08d380a06dc2ea643159c77
kwan78 <tei_tokyo@naver.com>
2012-08-17 18:22:41 +0900

    교재 테스트 134페이지

--- alex_app1/ViewController.h
+++ alex_app1/ViewController.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *toggleControls;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
+
+- (IBAction)textFildDoneEditing:(id)sender;
+- (IBAction)backgroundTap:(id)sender;
+- (IBAction)sliderChanged:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)toggleControls:(id)sender;
+- (IBAction)buttonPressed:(id)sender;
 
 @end
--- alex_app1/ViewController.m
+++ alex_app1/ViewController.m
@@ -14,14 +14,36 @@
 
 @implementation ViewController
 
+@synthesize toggleControls;
+@synthesize doSomethingButton;
+@synthesize sliderLabel;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize nameField;
+@synthesize numberField;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *buttonImageNormal = [UIImage imageNamed:@"whiteButton.png"];
+    UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    
+    UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
+    UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
 }
 
 - (void)viewDidUnload
 {
+    [self setNameField:nil];
+    [self setNumberField:nil];
+    [self setSliderLabel:nil];
+    [self setLeftSwitch:nil];
+    [self setRightSwitch:nil];
+    [self setToggleControls:nil];
+    [self setDoSomethingButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -34,5 +56,66 @@
         return YES;
     }
 }
+ 
+- (IBAction)textFildDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+}
 
+- (IBAction)backgroundTab:(id)sender{
+    [nameField resignFirstResponder];
+    [numberField resignFirstResponder];
+    
+}
+- (IBAction)sliderChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    int progressAsInt = (int)roundf(slider.value);
+    sliderLabel.text = [NSString stringWithFormat:@"%d", progressAsInt];
+}
+- (IBAction)switchChanged:(id)sender {
+    UISwitch *whichSwitch = (UISwitch *)sender;
+    BOOL setting = whichSwitch.isOn;
+    [leftSwitch setOn:setting animated:YES];
+    [rightSwitch setOn:setting animated:YES];
+    
+}
+
+- (IBAction)toggleControls:(id)sender {
+    if ([sender selectedSegmentIndex] == 0){
+        leftSwitch.hidden = NO;
+        rightSwitch.hidden = NO;
+        doSomethingButton.hidden = YES;
+    }
+    else {
+        leftSwitch.hidden = YES;
+        rightSwitch.hidden = YES;
+        doSomethingButton.hidden = NO;
+    }
+}
+
+    
+- (IBAction)buttonPressed:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" delegate:self cancelButtonTitle:@"No Way!!" destructiveButtonTitle:@"Yes, I'm Sure!" otherButtonTitles:nil];
+                          
+    [actionSheet showInView:self.view];
+}
+                                  
+                                  
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+    {
+        if(buttonIndex != [actionSheet cancelButtonIndex])
+        {
+            NSString *msg = nil;
+        
+            if (nameField.text.length > 0)
+                msg = [[NSString alloc] initWithFormat:@"You can breathe easy, %@, everything went OK.",nameField.text];
+            else
+                msg = @"You can breathe easy, everything went OK.";
+                
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something was done" message:msg delegate:self cancelButtonTitle:@"됐어!"otherButtonTitles:@"잘했어",nil];
+            
+                [alert show];
+        }
+    }
+        
 @end
+        
